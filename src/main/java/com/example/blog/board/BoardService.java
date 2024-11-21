d122d4c7b92123052f049c210a24b17db41c3ab9
getinthere <getinthere@naver.com>
2024-11-21 11:59:59 +0900

    ê·글로벌 예외처리

--- src/main/java/com/example/blog/board/BoardService.java
+++ src/main/java/com/example/blog/board/BoardService.java
@@ -1,5 +1,6 @@
 package com.example.blog.board;
 
+import com.example.blog._core.error.ex.Exception404;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,14 +21,14 @@ public class BoardService {
 
     public BoardResponse.UpdateFormDTO 게시글수정화면보기(int id) {
         Board board = boardRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("해당 id의 게시글이 없습니다 : "+id));
+                .orElseThrow(() -> new Exception404("해당 id의 게시글이 없습니다 : "+id));
 
         return new BoardResponse.UpdateFormDTO(board);
     }
 
     public BoardResponse.DetailDTO 게시글상세보기(int id) {
         Board board = boardRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("해당 id의 게시글이 없습니다 : "+id));
+                .orElseThrow(() -> new Exception404("해당 id의 게시글이 없습니다 : "+id));
         return new BoardResponse.DetailDTO(board);
     }
 
@@ -44,7 +45,7 @@ public class BoardService {
     @Transactional
     public void 게시글수정하기(int id, BoardRequest.UpdateDTO updateDTO) {
         Board board = boardRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("해당 id의 게시글이 없습니다 : "+id));
+                .orElseThrow(() -> new Exception404("해당 id의 게시글이 없습니다 : "+id));
 
         board.update(updateDTO.getTitle(), updateDTO.getContent());
     } // 영속화된 객체상태변경 - update + commit   => 더티체킹
