8115507a4328eae575b7392eeed9271407e704cd
getinthere <getinthere@naver.com>
2024-11-29 14:27:21 +0900

    로그인 완료

--- src/main/java/com/example/blog/_core/error/MyControllerAdvice.java
+++ src/main/java/com/example/blog/_core/error/MyControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.example.blog._core.error;
 
 import com.example.blog._core.error.ex.Exception400;
+import com.example.blog._core.error.ex.Exception401;
 import com.example.blog._core.error.ex.Exception404;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,7 +14,6 @@ public class MyControllerAdvice {
     @ResponseBody
     @ExceptionHandler(Exception400.class)
     public String err400(Exception400 e){
-        System.out.println("err400");
         String body = """
                 <script>
                     alert('${msg}');
@@ -25,9 +25,20 @@ public class MyControllerAdvice {
     }
 
     @ResponseBody
+    @ExceptionHandler(Exception401.class)
+    public String err401(Exception401 e){
+        String body = """
+                <script>
+                    alert('${msg}');
+                    location.href='/login-form';
+                </script>
+                """.replace("${msg}",e.getMessage());
+        return body;
+    }
+
+    @ResponseBody
     @ExceptionHandler(Exception404.class)
     public String err404(Exception404 e){
-        System.out.println("err404");
         String body = """
                 <script>
                     alert('${msg}');
--- src/main/java/com/example/blog/user/UserService.java
+++ src/main/java/com/example/blog/user/UserService.java
@@ -1,4 +1,19 @@
 package com.example.blog.user;
 
+import com.example.blog._core.error.ex.Exception401;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    
+    public User 로그인(UserRequest.LoginDTO loginDTO){
+        User userPS = userRepository.findByUsername(loginDTO.getUsername());
+        if(!userPS.getPassword().equals(loginDTO.getPassword())){
+            throw new Exception401("비밀번호가 일치하지 않습니다");
+        }
+        return userPS;
+    }
 }
